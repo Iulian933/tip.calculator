b6db77ffa65f2e10c09d50588a04994ac7ae7000
Iulian933 <iulian.acsinte93@gmail.com>
2022-01-14 07:36:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-function calcPercent () {
+const calcPercent = () => {
     let billValue = document.getElementById('bill').value;
     let billPersons = document.getElementById('people').value;
+    let result = document.getElementById('result');
+    let resultTotal = document.getElementById('result-total');
+    
     let calcTip = billValue * 0.05;
     let amountPersons = calcTip / billPersons;
-    console.log(amountPersons)
+    resultTotal.innerText = billValue / billPersons + amountPersons;
+    result.innerText = amountPersons;
 }
