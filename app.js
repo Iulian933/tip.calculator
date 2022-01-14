a1648336be076b8ed704e2fb9cdd524496b59dd3
Iulian933 <iulian.acsinte93@gmail.com>
2022-01-14 06:13:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-const bill = document.getElementById('bill');
-const people = document.getElementById('people');
-
+function calcPercent () {
+    let billValue = document.getElementById('bill').value;
+    let billPersons = document.getElementById('people').value;
+    let calcTip = billValue * 0.05;
+    let amountPersons = calcTip / billPersons;
+    console.log(amountPersons)
+}
